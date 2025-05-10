6165fc610d221dc297043f4fce1ded9306af9ee6
Pratyush <choudharypratyush809@gmail.com>
2025-05-10 11:37:50 +0530

    closed

--- src/pages/Closed.jsx
+++ src/pages/Closed.jsx
@@ -9,9 +9,9 @@ const Closed = () => {
         <h2 className="subtitle">Team OSS</h2>
         
         <div className="project-info">
-          <p>Registration for Commit 2.0 is now closed.</p>
-          <p>Thank you for your interest!</p>
-          <p>For onspot registration, contact us at:</p>
+          <p>Registration for COMMIT 2.0 is now officially closed.</p>
+          <p>Thank you for your overwhelming interest and support!</p>
+          <p>For updates and future events, stay connected with us:</p>
           <p className='text-[#60efff]'><a target='blank' href="https://www.instagram.com/team__oss/?hl=en">@team__oss</a></p>
           <p className='text-[#60efff]'> <a href="tel:+918840267636">+91-8840267636</a>  </p>
         </div>
