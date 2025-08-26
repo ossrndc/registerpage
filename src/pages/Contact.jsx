f9fba9d92ce97ad5e8efdf18025f44fa97c9f8fd
Pratyush <choudharypratyush809@gmail.com>
2025-08-26 17:53:42 +0530

    testing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const emailPattern = /^[A-Za-z]{3,}24\d{3}[A-Za-z0-9]{5}@akgec\.ac\.in$/;
+    const emailPattern = /^[A-Za-z]{3,}24\d{3}[A-Za-z0-9]*@akgec\.ac\.in$/;
     if (!emailPattern.test(contactData.Email)) {
       alert(
         "Please enter a valid AKGEC email address (e.g., yourname@akgec.ac.in)"
