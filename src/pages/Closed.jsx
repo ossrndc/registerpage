e41557661cbb8832dcd93440d5f46b0c31f22584
Pratyush <choudharypratyush809@gmail.com>
2026-02-24 09:03:59 +0530

    closed 3

--- src/pages/Closed.jsx
+++ src/pages/Closed.jsx
@@ -28,11 +28,6 @@ const Closed = () => {
           <p>Thank you for your overwhelming interest and support!</p>
           <p>For updates and future events, stay connected with us:</p>
 
-          <p className="text-red-600">
-            <strong>Important Update:</strong>
-            <br />
-            On-spot registrations will be available only for boys and day scholar girls.
-          </p>
 
           <p>
             <a
@@ -43,6 +38,11 @@ const Closed = () => {
             >
               @team__oss
             </a>
+          </p>
+          <p className="text-red-600">
+            <strong>📢Important Update:</strong>
+            <br />
+            On-spot registrations will be available only for boys and day scholar girls.
           </p>
         </div>
       </div>
